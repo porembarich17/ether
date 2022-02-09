a57efc48766f57f904648ee3cc5c0f32d0309146
Pascal Poremba <e11809911@student.tuwien.ac.at>
2022-02-09 22:12:52 +0100

    Private/Public key generation works via os.system with wireguard key gen

--- ether/sdn/wireguard_output.py
+++ ether/sdn/wireguard_output.py
@@ -1,9 +1,7 @@
 from string import Template
 import networkx as nx
-from cryptography.hazmat.primitives import serialization as crypto_serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend as crypto_default_backend
-import base64
+import os
+
 
 from ether.topology import Topology
 from ether.core import Node, Link, Connection, Route, NetworkNode
@@ -18,8 +16,8 @@ def ips_to_string(ip_list):
 
 
 def create_output(topology):
-    conf_template_interface = Template("[Interface]\nAddress = $interface_address \nPrivateKey = $interface_private \nListenPort = $interface_port\n\n")
-    conf_template_peer = Template("[Peer]\nPublicKey = $peer_public \nEndpoint = $peer_endpoint \nAllowedIPs = $peer_allowed \nPersistentKeepalive = $peer_persistent_keep_alive\n\n")
+    conf_template_interface = Template("[Interface]\nAddress = $interface_address \nPrivateKey = $interface_private ListenPort = $interface_port\n\n")
+    conf_template_peer = Template("[Peer]\nPublicKey = $peer_public Endpoint = $peer_endpoint \nAllowedIPs = $peer_allowed \nPersistentKeepalive = $peer_persistent_keep_alive\n\n")
 
     links = [node for node in topology.nodes if isinstance(node, Link)]
     nodes = [node for node in topology.nodes if isinstance(node, Node)]
@@ -33,47 +31,29 @@ def create_output(topology):
 
     for l in links:
         #create rsa key pair for links
-        key = rsa.generate_private_key(
-            backend=crypto_default_backend(),
-            public_exponent=65537,
-            key_size=512
-        )
-
-        private_key = key.private_bytes(
-            crypto_serialization.Encoding.PEM,
-            crypto_serialization.PrivateFormat.PKCS8,
-            crypto_serialization.NoEncryption()
-        )
-
-        public_key = key.public_key().public_bytes(
-            crypto_serialization.Encoding.OpenSSH,
-            crypto_serialization.PublicFormat.OpenSSH
-        )
-
-        privateKeys.update({str(l.tags['name']): base64.b64encode(private_key).decode('ascii')})
-        publicKeys.update({str(l.tags['name']): base64.b64encode(public_key).decode('ascii')})
+        privatekey = ""
+        publickey = ""
+        os.system("wg genkey | tee privatekey | wg pubkey > publickey")
+        f = open("privatekey", "r")
+        privatekey = f.read()
+        f = open("publickey", "r")
+        publickey = f.read()
+        privateKeys.update({str(l.tags['name']): privatekey})
+        publicKeys.update({str(l.tags['name']): publickey})
 
     for n in nodes:
         #create rsa key pair for nodes
-        key = rsa.generate_private_key(
-            backend=crypto_default_backend(),
-            public_exponent=65537,
-            key_size=512
-        )
+        privatekey = ""
+        publickey = ""
+        os.system("wg genkey | tee privatekey | wg pubkey > publickey")
+        f = open("privatekey", "r")
+        privatekey = f.read()
+        f = open("publickey", "r")
+        publickey = f.read()
+        print("-----" + str(privatekey))
 
-        private_key = key.private_bytes(
-            crypto_serialization.Encoding.PEM,
-            crypto_serialization.PrivateFormat.PKCS8,
-            crypto_serialization.NoEncryption()
-        )
-
-        public_key = key.public_key().public_bytes(
-            crypto_serialization.Encoding.OpenSSH,
-            crypto_serialization.PublicFormat.OpenSSH
-        )
-
-        privateKeys.update({str(n): base64.b64encode(private_key).decode('ascii')})
-        publicKeys.update({str(n): base64.b64encode(public_key).decode('ascii')})
+        privateKeys.update({str(n): privatekey})
+        publicKeys.update({str(n): publickey})
 
         #privateKeys.update({str(n): private_key})
         #publicKeys.update({str(n): public_key})
